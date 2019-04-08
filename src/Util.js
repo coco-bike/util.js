f17721c7f5b316e9d23c050c20b32e4fed8b24a4
miaomiaomiao93 <444503829@qq.com>
2019-04-08 15:02:17 +0800

    获取菜单树的递归，冒泡算法

--- src/Util.js
+++ src/Util.js
@@ -745,8 +745,37 @@ var Util = {
 	//     document.getElementsByTagName('head')[0].appendChild(script);
 	// },
 
+	/************************************************************************
+	 * 算法
+	 ************************************************************************/
+	//递归获取菜单树
+	setTreeData: (source) => {
+		let cloneData = JSON.parse(JSON.stringify(source))
+		let tree = cloneData.filter(father => {
+			let branchArr = cloneData.filter(child => {
+				return father.id == child.parentId
+			});
+			if (branchArr.length > 0) {
+				father.children = branchArr
+			}
+			return father.parentId == 0 //如果第一层不是parentId=0，请自行修改
+		})
+		return tree
+	},
 
-
+	//冒泡排序
+	sort: (elements) => {
+		for (let i = 0; i < elements.length - 1; i++) {
+			for (let j = 0; j < elements.length - i - 1; j++) {
+				if (elements[j].orderSort > elements[j + 1].orderSort) {
+					let swap = elements[j];
+					elements[j] = elements[j + 1];
+					elements[j + 1] = swap;
+				}
+			}
+		}
+		return elements;
+	},
 
 	/************************************************************************
 	 * 其他
